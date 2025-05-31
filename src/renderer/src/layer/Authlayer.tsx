f308e4156a2c7c0407c553dcc7406c80eb8e4a5d
Meraj Alam <merajansari81034@gmail.com>
2025-05-31 22:52:43 +0530

    Create root layer

---
+++ src/renderer/src/layer/Authlayer.tsx
@@ -0,0 +1,28 @@
+import Sidebar from '@renderer/components/drawer/Sidebar'
+import { useEffect, useState } from 'react'
+import { Outlet } from 'react-router-dom'
+
+const Authlayer = (): React.ReactElement => {
+  const [height, setHeight] = useState(window.innerHeight)
+
+  useEffect(() => {
+    const handleResize = ():void => setHeight(window.innerHeight)
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  return (
+    <div className="dark:bg-gray-900 p-1 pr-0 md:pr-1">
+      <div className="flex dark:text-white pt-1">
+        <Sidebar />
+        <div className="flex-1 md:pl-2 pr-1 overflow-auto" style={{ height: height }}>
+          <div className="dark:bg-gray-800 md:p-5 rounded-xl">
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Authlayer
