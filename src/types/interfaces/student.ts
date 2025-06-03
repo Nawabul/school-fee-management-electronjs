a5a3bb7eafbba42d2942c091bc3797b87dda1485
Nawabul <nawabk693@gmail.com>
2025-06-03 17:29:54 +0530

    Add: Student ts in main

---
+++ src/types/interfaces/student.ts
@@ -0,0 +1,39 @@
+export interface Student_Write {
+  reg_number: string
+  student_name: string
+  father_name: string
+  mobile: string
+  is_whatsapp: boolean
+  admission_date: string // ISO date string
+  address: string
+  class_id: number
+  initial_balance: number
+}
+export interface Student_Record {
+  id: number
+  reg_number: string
+  student_name: string
+  father_name: string
+  mobile: string
+  admission_date: string // ISO date string
+  transfer_date?: string | 'active' // either an ISO date string or the literal "active"
+  address: string
+  class_name: string
+}
+
+export interface Student_Get {
+  id: number
+  reg_number: string
+  student_name: string
+  father_name: string
+  mobile: string
+  is_whatsapp: boolean
+  admission_date: string // ISO date string
+  transfer_date: string | null // either an ISO date string or the literal "active"
+  address: string
+  class_id: number
+  initial_balance: number
+  current_balance: number
+  last_fee_date: string // ISO date string
+  last_notification_date: string // ISO date string
+}
