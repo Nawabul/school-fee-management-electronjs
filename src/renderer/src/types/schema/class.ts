75bbbb6770adc4fa2208463731a531daa3cd626a
Nawabul <nawabk693@gmail.com>
2025-06-04 18:17:33 +0530

    Modify: Class ts pretter

--- src/renderer/src/types/schema/class.ts
+++ src/renderer/src/types/schema/class.ts
@@ -1,11 +1,15 @@
-import { z } from "zod";
+import { z } from 'zod'
 
 export const Class_Schema = z.object({
-  name: z.string({
-    required_error: "Class name is required",
-  }).min(1, "Class name must be at least 1 character long"),
-  amount: z.coerce.number({
-    required_error: "Amount is required",
-    invalid_type_error: "Amount must be a valid number",
-  }).min(0, "Amount must be a positive number"),
-});
+  name: z
+    .string({
+      required_error: 'Class name is required'
+    })
+    .min(1, 'Class name must be at least 1 character long'),
+  amount: z.coerce
+    .number({
+      required_error: 'Amount is required',
+      invalid_type_error: 'Amount must be a valid number'
+    })
+    .min(0, 'Amount must be a positive number')
+})
