a66a15e3d12d717b86bb01317139400cd3362fd2
Nawabul <nawabk693@gmail.com>
2025-06-04 18:15:38 +0530

    Add: Payment insert update record comopnent

---
+++ src/renderer/src/pages/payment/PaymentInsert.tsx
@@ -0,0 +1,50 @@
+import { useMutation } from '@tanstack/react-query'
+import { Button } from 'flowbite-react'
+import { HiAcademicCap } from 'react-icons/hi'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+import PaymentController from '@renderer/controller/PaymentController'
+import PaymentForm from '@renderer/components/payment/PaymentForm'
+
+const PaymentInsert = (): React.JSX.Element => {
+  const navigate = useNavigate()
+
+  const studentId = useParams<{ id: string }>().id
+
+  const paymentMutation = useMutation({
+    mutationKey: ['payment', 'insert'],
+    mutationFn: (data) => PaymentController.create(studentId, data),
+    onSuccess: () => {
+      navigate(-1)
+      // Optionally reset form or show success message
+    },
+    onError: (error) => {
+      console.error('Error creating class:', error)
+      // Optionally show error message
+    }
+  })
+
+  const handleFormSubmit = (data): void => {
+    paymentMutation.mutate(data)
+  }
+
+  return (
+    <div>
+      <div className="flex gap-2 justify-between items-center mb-4 bg-gray-700 rounded-t-xl md:p-5">
+        <div className="flex gap-2 items-center">
+          <HiAcademicCap size={40} />
+          <h1 className="text-2xl font-bold">Student Insert</h1>
+        </div>
+        <div>
+          <Link to={`payment/${studentId}`}>
+            <Button>View All Payment</Button>
+          </Link>
+        </div>
+      </div>
+      <div className="md:p-5">
+        <PaymentForm onSubmit={handleFormSubmit} isPending={paymentMutation.isPending} />
+      </div>
+    </div>
+  )
+}
+
+export default PaymentInsert
---
+++ src/renderer/src/pages/payment/PaymentUpdate.tsx
@@ -0,0 +1,73 @@
+import PaymentForm from '@renderer/components/payment/PaymentForm'
+import PaymentController from '@renderer/controller/PaymentController'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { Button } from 'flowbite-react'
+import { Loader2 } from 'lucide-react'
+import { useEffect } from 'react'
+import { HiAcademicCap } from 'react-icons/hi'
+import { useNavigate, useParams } from 'react-router-dom'
+
+const PaymentUpdate = (): React.JSX.Element => {
+  const id = useParams().id
+  const navigate = useNavigate()
+  const studentMutation = useMutation({
+    mutationFn: (data) => PaymentController.update(Number(id), data),
+    onSuccess: () => {
+      // Optionally reset form or show success message
+      navigate(-1)
+    },
+    onError: (error) => {
+      console.error('Error creating payment:', error)
+      // Optionally show error message
+    }
+  })
+
+  const handleFormSubmit = (data): void => {
+    // Post to backend or handle in state
+    studentMutation.mutate(data)
+  }
+  const key = ['payment-single', id]
+  const { data, isLoading, isSuccess, isError, error } = useQuery({
+    queryKey: key,
+    queryFn: () => PaymentController.fetch(Number(id)),
+    enabled: !!id, // Only run this query if id is available
+
+    refetchOnWindowFocus: 'always'
+  })
+  const queryClient = useQueryClient()
+  useEffect(() => {
+    return () => {
+      queryClient.removeQueries({
+        queryKey: key,
+        exact: true
+      })
+    }
+  }, [])
+  return (
+    <div>
+      <div className="flex gap-2 justify-between items-center mb-4 bg-gray-700 rounded-t-xl md:p-5">
+        <div className="flex gap-2 items-center">
+          <HiAcademicCap size={40} />
+          <h1 className="text-2xl font-bold">Update Payment</h1>
+        </div>
+        <div>
+          <Button onClick={() => navigate(-1)}>View All Student</Button>
+        </div>
+      </div>
+      <div className="md:p-5">
+        {isLoading && <Loader2 className="animate-spin h-5 w-5 text-gray-500" />}
+        {isError && <p className="text-red-500">Error: {error.message}</p>}
+        {isSuccess && (
+          <PaymentForm
+            onSubmit={handleFormSubmit}
+            isPending={studentMutation.isPending}
+            // @ts-ignore working well response type
+            defaultValues={data}
+          />
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default PaymentUpdate
