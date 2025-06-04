b546a186e7dff98f2945bc3ab26f0e460c3f4d4c
Nawabul <nawabk693@gmail.com>
2025-06-04 12:32:37 +0530

    Add: Mis Item Schema

---
+++ src/main/db/schema/mis_item.ts
@@ -0,0 +1,7 @@
+import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core'
+
+export const mis_items = sqliteTable('mis_items', {
+  id: integer('id').primaryKey({ autoIncrement: true }),
+  name: text('date').notNull(), // store ISO date string
+  amount: integer('amount').notNull().default(0)
+})
