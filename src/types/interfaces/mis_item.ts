d74c7a678704c9eb0a6a9c65eab0ea17267e8f3e
Nawabul <nawabk693@gmail.com>
2025-06-04 13:25:59 +0530

    Add: Mis. Item Service

---
+++ src/types/interfaces/mis_item.ts
@@ -0,0 +1,15 @@
+export interface Mis_Item_Write {
+  name: string
+  amount: number
+}
+
+export interface Mis_Item_Record {
+  id: number
+  name: string
+  amount: number
+}
+
+export interface Mis_Item_Read {
+  name: string
+  amount: number
+}
