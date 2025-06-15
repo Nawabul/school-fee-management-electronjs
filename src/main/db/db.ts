5036f24bbe176802d9f39e1ac76acac35ca12fb5
Nawabul <nawabk693@gmail.com>
2025-06-15 13:45:06 +0530

    Modify: Db path in user data

--- src/main/db/db.ts
+++ src/main/db/db.ts
@@ -1,7 +1,12 @@
 import { drizzle } from 'drizzle-orm/better-sqlite3'
 import Database from 'better-sqlite3'
+import { join } from 'path'
+import { app } from 'electron'
 
-const sqlite = new Database(process.env.DB_FILE_NAME || 'school.db')
+const db_name = process.env.DB_FILE_NAME || 'school.db'
+const user_path = app.getPath('userData')
+const path = join(user_path, db_name)// path to the database file
+const sqlite = new Database(path)
 const db = drizzle({ client: sqlite })
 
 // @ts-ignore row db exported
