a61fdb0c5fa3f36a70e2053f98a6eb72778f61b3
Nawabul <nawabk693@gmail.com>
2025-06-28 11:54:35 +0530

    Add: Admission charge column add in class

--- src/main/db/schema/class.ts
+++ src/main/db/schema/class.ts
@@ -3,5 +3,6 @@ import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core'
 export const classes = sqliteTable('classes', {
   id: integer('id').primaryKey({ autoIncrement: true }),
   name: text('name').notNull().unique(),
-  amount: integer('amount').notNull().default(0)
+  amount: integer('amount').notNull().default(0),
+  admission_charge: integer('admission_charge').notNull().default(0)
 })
