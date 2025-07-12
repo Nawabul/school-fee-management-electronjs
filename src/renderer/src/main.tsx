23b9648c12f362cba9231108d73f239ca722e368
Nawabul <nawabk693@gmail.com>
2025-07-12 19:08:05 +0530

    Modify: Student finanace structure ui

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client'
 import Router from './Router'
 import { createTheme, ThemeProvider } from 'flowbite-react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ModelProvider } from './hooks/useModel'
 
 const customTheme = createTheme({
   button: {
@@ -19,8 +20,10 @@ const customTheme = createTheme({
 const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <ThemeProvider theme={customTheme}>
-  <QueryClientProvider client={queryClient}>
-    <Router />
-  </QueryClientProvider>
+    <ModelProvider>
+      <QueryClientProvider client={queryClient}>
+        <Router />
+      </QueryClientProvider>
+    </ModelProvider>
   </ThemeProvider>
 )
