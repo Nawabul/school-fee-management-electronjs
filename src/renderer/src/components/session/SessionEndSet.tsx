f9f5cd7d104dbabadc511c6d32a2e0ad66f0754f
Meraj Alam <merajansari81034@gmail.com>
2025-07-14 12:03:44 +0530

    Modify: Update SessionEndSet component to add btnStyle prop and improve form layout and styling

--- src/renderer/src/components/session/SessionEndSet.tsx
+++ src/renderer/src/components/session/SessionEndSet.tsx
@@ -6,8 +6,9 @@ import React, { useEffect, useState } from 'react'
 type props = {
   sumbitFun: () => void
   value?: string | number
+  btnStyle?: React.CSSProperties
 }
-function SessionEndSet({ sumbitFun, value = 3 }: props): React.ReactNode {
+function SessionEndSet({ sumbitFun, value = 3, btnStyle }: props): React.ReactNode {
   const [selectedMonth, setSelectedMonth] = useState<number>(Number(value))
   const months = [
     { id: 1, name: 'January' },
@@ -43,17 +44,17 @@ function SessionEndSet({ sumbitFun, value = 3 }: props): React.ReactNode {
   }
 
   return (
-    <form onSubmit={onSubmit} className="flex items-end gap-4">
+    <form onSubmit={onSubmit} className="w-full space-y-4">
       <div>
         <label
           htmlFor="session_end_month"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          className="block mb-1 text-start font-medium text-gray-900 dark:text-white"
         >
           Select an option
         </label>
         <select
           id="session_end_month"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="w-full px-4 py-2.5 bg-slate-700 border border-slate-600 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition appearance-none text-white focus:outline-none"
           onChange={(e) => {
             const value = e.target.value
             console.log(value)
@@ -67,7 +68,13 @@ function SessionEndSet({ sumbitFun, value = 3 }: props): React.ReactNode {
           ))}
         </select>
       </div>
-      <Button type="submit"> Submit</Button>
+      <Button
+        type="submit"
+        style={btnStyle}
+        className="w-full bg-blue-600 text-white font-bold py-3 px-4 rounded-lg"
+      >
+        Submit
+      </Button>
     </form>
   )
 }
