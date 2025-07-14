86357516d4169f5c3ee8d8d986912501b4c1c3d9
Meraj Alam <merajansari81034@gmail.com>
2025-07-14 12:02:34 +0530

    Modify: Refactor MisItemForm, PaymentForm, and StudentForm layouts for improved structure and styling

--- src/renderer/src/components/mis_item/MisItemFrom.tsx
+++ src/renderer/src/components/mis_item/MisItemFrom.tsx
@@ -23,24 +23,33 @@ const MisItemForm = ({
   })
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="grid grid-cols-1 md:grid-cols-3 gap-4 mx-auto p-6 rounded-lg"
-    >
-      {/* Class Name */}
-      <FormInput
-        name="name"
-        label="Name"
-        placeholder="e.g. Addmission Charge Class I 2025"
-        type="text"
-        control={control}
-      />
+    <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 mx-auto p-6 rounded-lg">
+      <div className="bg-slate-800 p-6 rounded-xl shadow-lg">
+        <h2 className="text-lg font-semibold text-white mb-6 border-b border-slate-700 pb-4">
+          Mis. Item Details
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          {/* Class Name */}
+          <FormInput
+            name="name"
+            label="Name"
+            placeholder="e.g. Addmission Charge Class I 2025"
+            type="text"
+            control={control}
+          />
 
-      {/* Class Fee */}
-      <FormInput name="amount" label="Fee" placeholder="e.g. 300" type="number" control={control} />
-
+          {/* Class Fee */}
+          <FormInput
+            name="amount"
+            label="Fee"
+            placeholder="e.g. 300"
+            type="number"
+            control={control}
+          />
+        </div>
+      </div>
       {/* Submit */}
-      <div className="md:col-span-2 mt-auto ml-auto">
+      <div className="flex justify-end pt-4">
         <Button type="submit" size="md" className="w-60">
           {isPending ? 'Submitting...' : 'Submit'}
         </Button>
