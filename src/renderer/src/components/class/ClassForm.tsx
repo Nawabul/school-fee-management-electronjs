73f71e1fe563ad286fce00f091c64f69e6d4ecf4
Meraj Alam <merajansari81034@gmail.com>
2025-07-14 11:59:07 +0530

    Modify: Refactor ClassForm layout for improved structure and styling

--- src/renderer/src/components/class/ClassForm.tsx
+++ src/renderer/src/components/class/ClassForm.tsx
@@ -5,6 +5,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 import { Class_Schema } from '@renderer/types/schema/class'
 import { JSX } from 'react'
+import { Loader2 } from 'lucide-react'
 interface Props {
   onSubmit: (data: any) => void
   defaultValues?: z.infer<typeof Class_Schema> | Record<string, never>
@@ -23,28 +24,36 @@ const ClassForm = ({
   })
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="grid grid-cols-1 md:grid-cols-3 gap-4 mx-auto p-6 rounded-lg"
-    >
-      {/* Class Name */}
-      <FormInput name="name" label="Name" placeholder="Class I" type="text" control={control} />
-
-      {/* Class Fee */}
-      <FormInput name="amount" label="Fee" placeholder="e.g. 300" type="number" control={control} />
-      {/* Class Admissin fee */}
-      <FormInput
-        name="admission_charge"
-        label="Admission Charge"
-        placeholder="e.g. 1250"
-        type="number"
-        control={control}
-      />
-
-      {/* Submit */}
-      <div className="md:col-span-2 mt-auto ml-auto">
-        <Button type="submit" size="md" className="w-60">
-          {isPending ? 'Submitting...' : 'Submit'}
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="bg-slate-800 p-6 rounded-xl shadow-lg">
+        <h2 className="text-lg font-semibold text-white mb-6 border-b border-slate-700 pb-4">
+          Class Information
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <FormInput name="name" label="Name" placeholder="Class I" type="text" control={control} />
+          <FormInput
+            name="amount"
+            label="Fee"
+            placeholder="e.g. 300"
+            type="number"
+            control={control}
+          />
+          <div className="md:col-span-2">
+            <FormInput
+              name="admission_charge"
+              label="Admission Charge"
+              placeholder="e.g. 1250"
+              type="number"
+              control={control}
+            />
+          </div>
+        </div>
+      </div>
+      <br />
+      {/* Submit Button */}
+      <div className="md:col-span-2 mt-auto flex justify-end">
+        <Button type="submit" size="md" className="w-50">
+          {isPending ? <Loader2 className="animate-spin" /> : 'Submit'}
         </Button>
       </div>
     </form>
